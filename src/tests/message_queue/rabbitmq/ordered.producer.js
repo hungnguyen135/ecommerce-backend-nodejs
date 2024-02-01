f85ab56bfc1ef68511e2cf76f147f8e4722e6a69
kevinhung135 <hungduy3n2k1@gmail.com>
2024-02-01 17:04:37 +0700

    update at 01/02/2024

---
+++ src/tests/message_queue/rabbitmq/ordered.producer.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const amqplib = require('amqplib')
+
+async function consumerOrderedMessage() {
+    const connection = await amqplib.connect('amqp://guest:guest@localhost')
+    const channel = await connection.createChannel()
+
+    const queueName = 'ordered-queue'
+    await channel.assertQueue(queueName, {
+        durable: true
+    })
+
+    for (let i = 0; i < 10; i++) {
+        const message = `ordered-queue-message::${i}`;
+        console.log(`message: ${message}`);
+        channel.sendToQueue(queueName, Buffer.from(message), {
+            persistent: true
+        })
+    }
+
+    setTimeout(() => {
+        connection.close()
+    }, 1000)
+}
+
+consumerOrderedMessage().catch(err => console.error(err))
---
+++ src/tests/mysql/sql.md
@@ -0,0 +1,19 @@
+// CREATE 
+CREATE TABLE test_table (
+    id INT NOT NULL,
+    name VARCHAR(255) DEFAULT NULL,
+    age INT DEFAULT NULL,
+    address VARCHAR(255) DEFAULT NULL,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+// CREATE PROCEDURE
+CREATE DEFINE=`kevinhung135`@`%` PROCEDURE `insert_data`()
+BEGIN 
+DECLARE max_id INT DEFAULT 1000000;
+DECLARE i INT DEFAULT 1;
+WHILE i <= max_id DO
+INSERT INTO test_table (id, name, age, address) VALUES (i, CONCAT('name', i), i%100, CONCAT('address', i));
+SET i = i + 1;
+END WHILE;
+END
